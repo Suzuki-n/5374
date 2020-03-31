14c1bac32aba5be5afeeed444da295520f733928
suzuki-n <vee00703@nifty.ne.jp>
2020-04-01 07:52:00 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -5,7 +5,7 @@
 
 var SVGLabel = false; // SVGイメージを使用するときは、true。用意できない場合はfalse。
 
-var MaxDescription = 1; // ごみの最大種類、９を超えない場合は変更の必要はありません。
+var MaxDescription = 19; // ごみの最大種類、９を超えない場合は変更の必要はありません。
 
 var MaxMonth = 5;
 
